a47c6384154be40bbfc8077894d87523fc3b645c
vitruveo-validators <157662422+vitruveo-validators@users.noreply.github.com>
2024-02-08 21:24:40 -0500

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "On_a_Mac_run_this_first" {
+  value = "chmod 400 validator.pem"
+}
+
+output "On_Windows_run_two_command_from_documentation" {
+  value = "Review the documentation guide"
+}
+
+output "On_both_Mac_and_Windows_run_this_command_adding_your_new_public_IP" {
+  value = "ssh -i validator.pem validatoradmin@<YOUR-PUBLIC-IP>"
+}
+
+output "Your_public_IP" {
+  value = azurerm_linux_virtual_machine.validator_vm.public_ip_address
+}
+
+
