1c1f603821c7721b14af744395b8bc17e71de54d
vitruveo-validators <157662422+vitruveo-validators@users.noreply.github.com>
2024-03-26 16:18:25 -0400

    Update outputs.tf

--- Windows/validator#3/outputs.tf
+++ Windows/validator#3/outputs.tf
@@ -1,5 +1,5 @@
 output "On_a_Mac_run_this_first" {
-  value = "chmod 400 validator2.pem"
+  value = "chmod 400 validator3.pem"
 }
 
 output "On_Windows_run_two_command_from_documentation" {
@@ -7,7 +7,7 @@ output "On_Windows_run_two_command_from_documentation" {
 }
 
 output "On_both_Mac_and_Windows_run_this_command_adding_your_new_public_IP" {
-  value = "ssh -i validator2.pem validatoradmin@<YOUR-PUBLIC-IP>"
+  value = "ssh -i validator3.pem validatoradmin@<YOUR-PUBLIC-IP>"
 }
 
 output "Your_public_IP" {
